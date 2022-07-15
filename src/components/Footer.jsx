572a00125bb25a4339e750750a891e2ada412e34
Alison Paico <alison.jpq@gmail.com>
2022-07-15 10:37:30 -0500

    agrege footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { Grid } from "@mui/material";
 
-function Footer() {
+const Footer = () => {
   return (
-      <div>
-      
+    <Grid container justifyContent="flex-end">
+      <div class="social-container">
+        <h3>Siguenos en redes sociales</h3>
+        <a href="https://www.youtube.com/" className="youtube social">
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="https://www.twitter.com/"
+          className="twitter social"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
       </div>
+    </Grid>
   );
-}
-
+};
 export default Footer;
