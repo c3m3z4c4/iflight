8b9a270d7dbbfe9baf585fa9c7efcc1fd1666a62
claunicole <claunicole.gf@gmail.com>
2022-07-20 10:42:15 -0400

    Creacion de rutas

--- src/components/FlightDetail/FlightDetail.jsx
+++ src/components/FlightDetail/FlightDetail.jsx
@@ -1,14 +1,49 @@
 
+import "./FlightDetail.css"
+
 function FlightDetail({origin, destination, price}) {
   return (
-    <div>
-        <h2>Informacion del vuelo</h2>
-        <div>
-            <div>Itinerario: {origin}-{destination}</div>
-            <div>
-                <p>Precio ${price}</p>
+    <div className="detail-container">
+        <h2>Tu vuelo seleccionado:</h2>
+        <div className="origin-container">
+          <p>Vuelo de ida: fecha de ida </p>
+          <div className="flight-origin">
+            <div className="origin-init-detail">
+                 <div>{origin}</div>
+                 <div> 20:06</div>
             </div>
+            <div className="origin-route-detail">
+                  <p>Duracion</p>
+                  <hr/>
+                  <p>Sin escalas</p>
+            </div>
+            <div className="origin-fin-detail">
+                  <div>{destination}</div>
+                  <div> 07:05</div>
+            </div>
+          </div>
         </div>
+        <div className="destination-container">
+            <p>Vuelo de vuelta: fecha de vuelta </p>
+            <div className="flight-destination">
+              <div className="destination-init-detail">
+                <div>{destination}</div>
+                <div> 18:00</div>
+              </div>
+            <div className="destination-route-detail">
+                  <p>Duracion</p>
+                  <hr/>
+                  <p>Sin escalas</p>
+           </div>
+           <div className="destination-fin-detail">
+                  <div>{origin}</div>
+                  <div> 04:42</div>
+          </div>
+        </div>
+        <div>
+            <p>Precio total ${price}</p>
+        </div>
+    </div>
     </div>
   )
 }
