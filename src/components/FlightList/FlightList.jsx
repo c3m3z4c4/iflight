41975e7efe768f1a7865f4cdc4f07fb713d5a728
claunicole <claunicole.gf@gmail.com>
2022-07-22 13:13:01 -0400

    Ruta funcional páginas 2 y 3

--- src/components/FlightList/FlightList.jsx
+++ src/components/FlightList/FlightList.jsx
@@ -4,7 +4,7 @@ import Flight from "../Flight/Flight"
 function FlightList({flights}) {
   return (
     <div>
-          {flights.map(flight => <Flight id={flight.id} origin={flight.inicio} destination={flight.final} price={flight.price}/>)}
+          {flights.map(flight => <Flight id={flight.id} flight={flight}/>)}
     </div>
   )
 }
